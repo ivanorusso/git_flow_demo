f079660388d7c9eadeb8ab417fc508059e33b75e
Ivano Russo <ivano.russo@openjawtech.com>
2017-11-24 15:41:43 +0000

    @@

---
+++ src/main/java/com/example/Another.java
@@ -0,0 +1,25 @@
+package com.example;
+
+/**
+ * This is a class.
+ */
+public class Another {
+
+  /**
+   * This is a constructor.
+   */
+  public Another() {
+
+  }
+
+  //TODO: Add javadoc comment
+  public String greet(String someone) {
+    return String.format("Hello, %s!", someone);
+  }
+  
+  public void ciao()
+  {
+	  
+	  System.out.println("CIAO!!!!!");
+  }
+}
